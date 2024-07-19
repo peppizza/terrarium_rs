f81a8d5c779fa5287d72d8e4ba356a93c9cf2ae5
Spencer Vess <spencervess@protonmail.com>
2024-07-19 11:11:47 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Terrarium rust devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            pkg-config
+            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+              targets = [ "thumbv6m-none-eabi"];
+            }))
+            probe-rs
+          ];
+        };
+      }
+    );
+}
